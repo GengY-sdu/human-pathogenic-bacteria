c3591678116c89865e1ef3def2413ac52615a2f5
GengY-sdu <128786333+GengY-sdu@users.noreply.github.com>
2024-12-23 21:13:20 +0800

    Add files via upload

---
+++ future_EMP/step5_test_model.R
@@ -0,0 +1,33 @@
+#step5:test model###############################################################
+rm(list = ls())
+library(caret)
+
+load(file = 'pathogen/future_EMP/history_dataset_nohuman.Rdata')
+Data <- history_dataset
+colnames(Data)[1] <- "Response"
+load("pathogen/future_EMP/rffuncs_future_terra_nohuman.rdata")
+optvar <- rfFuncs$optVariables
+max(rfFuncs$results$Rsquared)
+
+set.seed(44)
+load(file = 'pathogen/future_EMP/inTrain.Rdata')
+data_train <- Data[inTrain,]
+data_test <- Data[-inTrain,]
+cols = c("Response",optvar)
+data_train <- data_train[,..cols]
+data_test <- data_test[,..cols]
+trainx <- data_train[,-1]
+trainy <- data_train$Response
+
+DatVad <- data_test
+load(file = 'pathogen/future_EMP/custom_nohuman.rdata')
+plot_train <- data.frame(custom$pred$pred,custom$pred$obs)
+write.csv(plot_train,file = 'pathogen/future_EMP/train_future_set.csv',row.names = F)
+fit2 <- custom
+
+DatVad$Predict <- predict(fit2,newdata = DatVad[,-1])
+cor.test(DatVad$Response,DatVad$Predict)
+yardstick::rmse(DatVad,truth=Response,estimate=Predict)
+R2(DatVad$Response,DatVad$Predict)
+plot_test <- data.frame(DatVad$Predict,DatVad$Response)
+write.csv(plot_test,file = 'pathogen/future_EMP/test_future_set.csv',row.names = F)
